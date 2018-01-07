08f8bb7231546ca2fe2e0ff004c85aa584386b14
Sebastian Geisler <sgeisler@agdsn.de>
2018-01-07 15:42:15 +0100

    make copying back target optional

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,15 @@ use toml::Value;
 struct Opts {
     // workaround for "remote" argument when calling "cargo remote"
     _unused: String,
+
+    #[structopt(help = "cargo command that will be executed remotely")]
     command: String,
+
     #[structopt(short = "r", long = "remote", help = "remote ssh build server")]
-    remote: Option<String>
+    remote: Option<String>,
+
+    #[structopt(short = "c", long = "copy-back", help = "transfer the target folder back to the local machine")]
+    copy_back: bool
 }
 
 fn main() {
@@ -66,49 +72,46 @@ fn main() {
         })
     });
 
-    match options.command {
-        Build => {
-            info!("Transferring sources to build server.");
-            // transfer project to build server
-            Command::new("rsync")
-                .arg("-a")
-                .arg("--delete")
-                .arg("--info=progress2")
-                .arg("--exclude")
-                .arg("target")
-                .arg(format!("{}/", project_dir.to_string_lossy()))
-                .arg(format!("{}:/tmp/remote-build-{}/", build_server, project_name))
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .stdin(Stdio::inherit())
-                .output()
-                .expect("failed to transfer project to build server");
+    info!("Transferring sources to build server.");
+    // transfer project to build server
+    Command::new("rsync")
+        .arg("-a")
+        .arg("--delete")
+        .arg("--info=progress2")
+        .arg("--exclude")
+        .arg("target")
+        .arg(format!("{}/", project_dir.to_string_lossy()))
+        .arg(format!("{}:/tmp/remote-build-{}/", build_server, project_name))
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdin(Stdio::inherit())
+        .output()
+        .expect("failed to transfer project to build server");
 
-            let build_command = format!("cd /tmp/remote-build-{}/; $HOME/.cargo/bin/cargo build", project_name);
+    let build_command = format!("cd /tmp/remote-build-{}/; $HOME/.cargo/bin/cargo {}", project_name, options.command);
 
-            info!("Starting build process.");
-            Command::new("ssh")
-                .arg(&build_server)
-                .arg(build_command)
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .stdin(Stdio::inherit())
-                .output()
-                .expect("failed to build project");
+    info!("Starting build process.");
+    Command::new("ssh")
+        .arg(&build_server)
+        .arg(build_command)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .stdin(Stdio::inherit())
+        .output()
+        .expect("failed to build project");
 
-            info!("Transferring artifacts back to client.");
-            Command::new("rsync")
-                .arg("-a")
-                .arg("--delete")
-                .arg("--info=progress2")
-                .arg(format!("{}:/tmp/remote-build-{}/target/", build_server, project_name))
-                .arg(format!("{}/target/", project_dir.to_string_lossy()))
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .stdin(Stdio::inherit())
-                .output()
-                .expect("failed to transfer built project to client");
-
-        }
+    if options.copy_back {
+        info!("Transferring artifacts back to client.");
+        Command::new("rsync")
+            .arg("-a")
+            .arg("--delete")
+            .arg("--info=progress2")
+            .arg(format!("{}:/tmp/remote-build-{}/target/", build_server, project_name))
+            .arg(format!("{}/target/", project_dir.to_string_lossy()))
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .output()
+            .expect("failed to transfer built project to client");
     }
 }
